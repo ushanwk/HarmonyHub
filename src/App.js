48cc69de6dfebb85f79d45e080d53252b2597ae8
UshanKaushalya <Ushanwk22@gmail.com>
2024-02-19 22:59:51 +0530

    header creating

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import {Header} from "./assets/components/Header";
 
 function App() {
     return (
         <div className="App h-[100vh] w-full bg-[url('assets/image/Bg.png')] flex items-center justify-center">
 
-          <section className="w-[65%] h-[700px] bg-white rounded-[20px] bg-[url('assets/image/SecBg.png')] bg-cover border-2 border-white shadow-xl">
+            <section
+                className="w-[65%] h-[700px] bg-white rounded-[20px] bg-[url('assets/image/SecBg.png')] bg-cover border-4 border-white shadow-xl">
 
-          </section>
+                <Header />
+
+            </section>
 
         </div>
     );
--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,7 +1,20 @@
+import logo from '../image/Logo.png';
+import profile from '../image/Profile.png';
+import notification from '../image/NotificationIcon.png';
+import setting from '../image/Setting.png';
+
 export function Header() {
     return (
-        <header>
-
+        <header className="w-full px-[50px] py-[15px] flex justify-between">
+            <img src={logo} width={150} />
+            <div className="flex gap-5">
+                <div className="mt-4 w-[100px] h-[30px] bg-white rounded-[6px] flex items-center justify-center max-lg:hidden">
+                    <p className="text-[11px] font-semibold">Upgrade Plan</p>
+                </div>
+                <a><img src={setting} className="mt-5 w-[20px] h-[20px] cursor-pointer"/></a>
+                <img src={notification} className="mt-5 w-[20px] h-[20px] max-lg:hidden"/>
+                <img src={profile} className="mt-4 w-[30px] h-[30px] ml-4 max-lg:hidden"/>
+            </div>
         </header>
     )
 }
