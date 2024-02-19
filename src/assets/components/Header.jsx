482773a8b6d904ac8722fe04e7691e3f3f8a68e7
UshanKaushalya <Ushanwk22@gmail.com>
2024-02-19 23:08:26 +0530

    header creating

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -5,14 +5,14 @@ import setting from '../image/Setting.png';
 
 export function Header() {
     return (
-        <header className="w-full px-[50px] py-[15px] flex justify-between">
+        <header className="w-full px-[50px] py-[20px] flex justify-between max-sm:px-[10px]">
             <img src={logo} width={150} />
             <div className="flex gap-5">
-                <div className="mt-4 w-[100px] h-[30px] bg-white rounded-[6px] flex items-center justify-center max-lg:hidden">
+                <div className="mt-4 w-[100px] h-[30px] bg-white rounded-[6px] flex items-center justify-center max-lg:hidden hover:border-[1px] cursor-pointer">
                     <p className="text-[11px] font-semibold">Upgrade Plan</p>
                 </div>
-                <a><img src={setting} className="mt-5 w-[20px] h-[20px] cursor-pointer"/></a>
-                <img src={notification} className="mt-5 w-[20px] h-[20px] max-lg:hidden"/>
+                <a><img src={setting} className="mt-5 w-[20px] h-[20px] cursor-pointer hover:rotate-45"/></a>
+                <img src={notification} className="mt-5 w-[20px] h-[20px] cursor-pointer max-lg:hidden hover:rotate-12"/>
                 <img src={profile} className="mt-4 w-[30px] h-[30px] ml-4 max-lg:hidden"/>
             </div>
         </header>
