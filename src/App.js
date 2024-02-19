faead72cf0a4cd65c63eefd77ba1681a5a764027
UshanKaushalya <Ushanwk22@gmail.com>
2024-02-19 22:19:57 +0530

    Bg ready

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
     return (
         <div className="App h-[100vh] w-full bg-[url('assets/image/Bg.png')] flex items-center justify-center">
 
-          <section className="w-[70%] h-[700px] bg-white rounded-[20px] bg-[url('assets/image/SecBg.png')] bg-cover border-2 border-white shadow-xl">
+          <section className="w-[65%] h-[700px] bg-white rounded-[20px] bg-[url('assets/image/SecBg.png')] bg-cover border-2 border-white shadow-xl">
 
           </section>
 
