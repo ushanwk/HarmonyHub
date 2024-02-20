b36ff5a34fbd5f8328fac1f3cdf7e50264b80eab
UshanKaushalya <Ushanwk22@gmail.com>
2024-02-20 11:23:52 +0530

    song banner creating

--- src/assets/components/PlayingNowBanner.jsx
+++ src/assets/components/PlayingNowBanner.jsx
@@ -1,10 +1,23 @@
 import songBanner from '../image/SongBanner.png';
+import playIcon from '../image/Play.png';
+import nextIcon from '../image/Next.png';
+import backIcon from '../image/Back.png';
+import shuffleIcon from '../image/Shuffle.png';
+import listIcon from '../image/List.png';
 
 export function PlayingNowBanner() {
     return (
         <div>
-            <div className="w-[325px] h-[375px] bg-white rounded-[10px] shadow-xl p-2 flex-col">
+            <div className="w-[325px] bg-white rounded-[10px] shadow-xl p-2 flex-col max-xl:w-full">
                 <img src={songBanner} className="rounded-[8px]" />
+
+                <div className="h-14 w-full mt-2 flex justify-center items-center gap-5">
+                    <img src={shuffleIcon} className="w-[25px]"/>
+                    <img src={nextIcon} className="w-[25px] rotate-180"/>
+                    <img src={playIcon} className="w-[25px]"/>
+                    <img src={backIcon} className="w-[25px]"/>
+                    <img src={listIcon} className="w-[25px]"/>
+                </div>
             </div>
         </div>
     )
